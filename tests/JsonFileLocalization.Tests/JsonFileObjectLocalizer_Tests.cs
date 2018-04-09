b8df50237a45ed0f3592a4e0eb1d45bacad5a438
Vladislav Khapin <hapinvlad@gmail.com>
2018-04-09 17:19:39 +0300

    Removed test

--- tests/JsonFileLocalization.Tests/JsonFileObjectLocalizer_Tests.cs
+++ tests/JsonFileLocalization.Tests/JsonFileObjectLocalizer_Tests.cs
@@ -51,22 +51,4 @@ namespace JsonFileLocalization.Tests
 
 
     }
-
-    public class JsonFileObjectLocalizerFactory_Tests
-    {
-        [Fact]
-        public void Create_WhenCreatedFromType_ReturnsResource()
-        {
-            //Arrange
-            CultureInfo.CurrentUICulture = new CultureInfo("ru-RU");
-            var factory = TestJsonFileObjectLocalizerFactory.GetFactory(JsonFileCultureSuffixStrategy.TwoLetterISO6391AndCountryCode);
-            var localizer = factory.Create(typeof(IntArrayObject));
-
-            //Act
-            var result = localizer.GetLocalizedObject<int[]>("Value");
-
-            //Assert
-            result.Value.Should().Equal(8, 9, 10);
-        }
-    }
 }
