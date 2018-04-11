f52cbaa15741ac7caeb32fa46ae0f08d419febad
Vladislav Khapin <hapinvlad@gmail.com>
2018-04-11 11:46:32 +0300

    Added comments

--- src/JsonFileLocalization/Caching/ConcurrentDictionaryCache.cs
+++ src/JsonFileLocalization/Caching/ConcurrentDictionaryCache.cs
@@ -24,16 +24,32 @@ namespace JsonFileLocalization.Caching
             return _cache.GetOrAdd(key, new Lazy<TValue>(valueFactory(key))).Value;
         }
 
+        /// <summary>
+        /// Invalidate value in cache by key
+        /// </summary>
+        /// <param name="key">key</param>
         public virtual void Invalidate(TKey key)
         {
             _cache.TryRemove(key, out _);
         }
 
+        /// <summary>
+        /// Try add value to a cache
+        /// </summary>
+        /// <param name="key">key</param>
+        /// <param name="value">value</param>
+        /// <returns>if added successfully</returns>
         public virtual bool TryAdd(TKey key, TValue value)
         {
             return _cache.TryAdd(key, new Lazy<TValue>(value));
         }
 
+        /// <summary>
+        /// Try get value from a cache
+        /// </summary>
+        /// <param name="key">key</param>
+        /// <param name="value">value</param>
+        /// <returns>if assigned value to a <paramref name="value"/></returns>
         public virtual bool TryGet(TKey key, out TValue value)
         {
             if (_cache.TryGetValue(key, out var lazyValue))
