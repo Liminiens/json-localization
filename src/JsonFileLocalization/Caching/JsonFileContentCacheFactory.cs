036b91382fc362619e2197801517905f8b0ea629
Vladislav Khapin <hapinvlad@gmail.com>
2019-01-12 00:39:19 +0300

    Work on example

--- src/JsonFileLocalization/Caching/JsonFileContentCacheFactory.cs
+++ src/JsonFileLocalization/Caching/JsonFileContentCacheFactory.cs
@@ -5,13 +5,10 @@ namespace JsonFileLocalization.Caching
     /// <inheritdoc />
     public class JsonFileContentCacheFactory : IJsonFileContentCacheFactory
     {
-        private readonly ConcurrentDictionaryCache<string, IJsonFileContentCache> _cacheProvider =
-            new ConcurrentDictionaryCache<string, IJsonFileContentCache>();
-
         /// <inheritdoc />
         public IJsonFileContentCache Create()
         {
-            return _cacheProvider.GetOrAdd(Guid.NewGuid().ToString(), key => new JsonFileContentCache());
+            return new JsonFileContentCache();
         }
     }
 }
